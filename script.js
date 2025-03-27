e85e3aafeb81e94cd6bf3316f5dd51579b9b71c9
VitorDiamanteNegro <168202201+VitorDiamanteNegro@users.noreply.github.com>
2025-03-27 18:47:38 -0300

    a

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+let currentNumber;
 
+function generateNumber() {
+    currentNumber = Math.floor(Math.random() * 100);
+    document.getElementById("randomNumber").textContent = currentNumber;
+}
+
+function checkGuess(choice) {
+    const isEven = currentNumber % 2 === 0;
+    const resultBox = document.getElementById("resultBox");
+    if ((choice === 'par' && isEven) || (choice === 'impar' && !isEven)) {
+        resultBox.textContent = "Einstein? slk, acertou!";
+        resultBox.style.color = "green";
+        
+    } else {
+        resultBox.textContent = "'Jênio' ! Tente novamente.";
+        resultBox.style.color = "red";
+        
+    }
+    generateNumber();
+}
+
+
+generateNumber();
+createStars();
+
+// estrelas
+function createStars() {
+    const starField = document.createElement("div");
+    starField.classList.add("stars");
+    document.body.appendChild(starField);
+    
+    const numberOfStars = 200;
+    for (let i = 0; i < numberOfStars; i++) {
+        const star = document.createElement("div");
+        star.classList.add("star");
+        const size = Math.random() * 3 + 1;
+        const positionX = Math.random() * 100 + "%";
+        const positionY = Math.random() * 100 + "%";
+        const duration = Math.random() * 2 + 1; 
+
+        star.style.width = `${size}px`;
+        star.style.height = `${size}px`;
+        star.style.left = positionX;
+        star.style.top = positionY;
+        star.style.animationDuration = `${duration}s`;
+
+        starField.appendChild(star);
+    }
+}
